1762d28d808ec62846268af14bc2a7b38f302596
Eric <ericmina83@gmail.com>
2016-03-27 00:43:37 +0800

    #intermediate task <https://laravel.com/docs/5.2/quickstart-intermediate#database-migrations> * create migration go creating tasks table * create Model:Task of tasks table

---
+++ database/migrations/2016_03_26_163440_create_tasks_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->increments('id');   //id
+            $table->integer('user_id')->index();    //加入關聯
+            $table->string('name'); //任務名稱
+            $table->timestamps();   //增加時間戳記
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('tasks');
+    }
+}
